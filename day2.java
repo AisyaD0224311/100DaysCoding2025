7356f3d2044be2ee56fb81439b1cdfe61ea059e3
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-09 11:17:13 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day2 {
+    public static void main(String[] args) {
+        String nama = "Aisya";
+        System.out.print("Nama : ");
+        System.out.println(nama);
+        System.out.printf("Nama saya %s" , nama);
+    }
+}
