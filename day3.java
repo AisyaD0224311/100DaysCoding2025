60bd6a096b4f4d11eeac53b6c574643a540112ce
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-10 14:08:09 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg100dayscoding2025;
+
+/**
+ *
+ * @author ASUS
+ */
+public class day3 {
+    //ini adalah method main
+    public static void main(String[] args) {
+        /*
+        Kegunaan print, untuk menampilkan output di console
+        */
+    }
+}
