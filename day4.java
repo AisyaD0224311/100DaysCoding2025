7a099aab591eb0310005e099168673c8e2bf7173
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-11 21:09:34 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,20 @@
+package pkg100dayscoding2025;
+public class day4 {
+    public static void main(String[] args) {
+        // \n untuk spasi
+        System.out.println("Halo\nNama saya Aisya");
+        
+        // \t untuk tab
+        System.out.println("Umur :\t18");
+        
+        // \" & \'menampilkan tanda kutip
+        System.out.println("\"Saya tinggal didesa paku\"");
+        System.out.println("\'Tepatnya di dusun passubbe\'");
+        
+        // \\menampilkan backlash
+        System.out.println("Folder : C:\\Users\\Aisya\\Documents");
+        
+        //\b menghapus satu karakter sebelumnya
+        System.out.println("Saya darii\b kelas informatika B");
+    }
+}
