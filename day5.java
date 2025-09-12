82e2c0c724b48e2e8703d463f008b13e62d70858
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-12 11:24:19 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding2025;
+public class day5 {
+    public static void main(String[] args) {
+        byte umur = 18;
+        int musik = 1000;
+        short semester = 3;
+        long gaji = 10000000L;
+        
+        System.out.println("Umur saya : " + umur);
+        System.out.println("Saya telah mendengar sebanyak " + musik + " lebih musik");
+        System.out.println("Saat ini saya semester " + semester);
+        System.out.println("Saya ingin mempunyai gaji sebanyak " + gaji + " rupiah perbulan");
+    }
+}
