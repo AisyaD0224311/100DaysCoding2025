96b44aa97469c23cecc594c3d6034acce8360f06
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-13 23:16:18 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding2025;
+public class day6 {
+    public static void main(String[] args) {
+        double gaji = 10.030;
+        float gaji2 = 7.25F;
+        
+        System.out.println("Upah Minimum Regional (UMR) di Korea Selatan untuk tahun 2025 adalah " + gaji + " won per jam");
+        System.out.println("Upah minimum federal adalah " + gaji2 + "$ per jam untuk pekerja yang tercakup oleh FLSA");
+    }
+}
