27e53ac37bf8b61c74660b61abdbba3e44be833c
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-14 16:12:35 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day7 {
+    public static void main(String[] args) {
+        char nilai = 'E';
+        boolean lulus = false;
+        
+        System.out.println("saya mendapatkan nilai " + nilai + " di dasar dasar pemrograman");
+        System.out.println("Apakah saya lulus di mata kuliah dasar Dasar Pemrograman? : " + lulus);
+    }
+    
+}
