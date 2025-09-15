25c3c01fb3239d1d28ba11fd9418ef1b61772210
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-15 17:46:18 +0800

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding2025;
+public class day8 {
+    public static void main(String[] args) {
+        String nama = "Aisya";
+        String nim = "D0224311";
+        String alamat = "Paku";
+        
+        System.out.println("Perkenalkan nama saya " + nama + ", \nDengan NIM " + nim + ", \nAlamat " + alamat);
+    }
+}
