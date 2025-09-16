a5eb47d2fc37fc44fd109e550b532968fa48c80b
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-16 22:01:54 +0800

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,20 @@
+package pkg100dayscoding2025;
+public class day9 {
+    public static void main(String[] args) {
+        String nama, nim;
+        int umur, semester;
+        
+        nama = "Aisya";
+        umur = 18;
+        nim = "D0224311";
+        semester = 3;
+        
+        System.out.println("Nama saya " + nama);
+        System.out.println("Umur saya " + umur);
+        System.out.println("NIM saya " + nim);
+        System.out.println("Semester " + semester);
+        
+        nama = "Aisyaaa";
+        System.out.println("Nama setelah di update : " + nama);
+    }
+}
