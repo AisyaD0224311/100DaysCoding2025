d8b57944cfac455b917ebb8d54589d786d0cd1bb
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-17 20:18:37 +0800

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day10 {
+    public static void main(String[] args) {
+        final String nama = "Aisya";
+        final int umur = 18;
+        System.out.println("Final nama : " + nama);
+        System.out.println("Final umur : " + umur);
+    }
+}
