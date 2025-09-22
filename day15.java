e5ab9bfceaa495608d60a2274274cd58b9f6c7db
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-22 16:07:41 +0800

    Add files via upload

---
+++ day15.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Hasil penjumlahan : " + (a + b));
+        System.out.println("Hasil pengurangan : " + (a - b));
+    }
+}
