8945f5675f89c582c72438856bcb111f99adfb5f
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-24 20:27:09 +0800

    Add files via upload

---
+++ day17.java
@@ -0,0 +1,8 @@
+package pkg100dayscoding2025;
+public class day17 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        System.out.println("Hasil Modulo : " + (a % b));
+    }
+}
