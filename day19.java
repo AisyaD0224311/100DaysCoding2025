44cd45138105b4dacbc5ba871b3cc9f39e11112d
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-26 20:14:39 +0800

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day19 {
+    public static void main(String[] args) {
+        double nilaiDouble = 9.78;
+        int nilaiInt = (int) nilaiDouble;
+        System.out.println("Nilai double : " + nilaiDouble);
+        System.out.println("Nilai double setelah di konversi ke int : " + nilaiInt);
+    }
+}
