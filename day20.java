78f4a107209a8d9b3fdd85f39cb0568040c8c928
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-27 21:59:44 +0800

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day20 {
+    public static void main(String[] args) {
+        int a = 1234;
+        String b = String.valueOf(a);
+        System.out.println("Angka sebelum diubah ke String : " + a);
+        System.out.println("Angka sesudah diubah ke String : " + b);
+    }
+}
