c70e37ab9f31b386fbddedc31b70c3d614935b9d
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-29 18:12:19 +0800

    Add files via upload

---
+++ day22.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day22 {
+    public static void main(String[] args) {
+        int s = 5;
+        int luas = s * s;
+        System.out.println("Sisi = " + s);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
