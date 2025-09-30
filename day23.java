4cfec90da7db8f342eacb340aed884748ef19ab3
AisyaD0224311 <wibusoft59@gmail.com>
2025-09-30 20:26:37 +0800

    Add files via upload

---
+++ day23.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day23 {
+    public static void main(String[] args) {
+        int p = 5;
+        int l = 10;
+        int luas = p * l;
+        System.out.println("Panjang : " + p);
+        System.out.println("Lebar : " + l);
+        System.out.println("Luas Persegi Panjang adalah : " + luas);
+    }
+}
