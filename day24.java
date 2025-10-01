01c7c564c239240e8815c498b90fd956b785e1c7
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-01 19:50:32 +0800

    Add files via upload

---
+++ day24.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day24 {
+    public static void main(String[] args) {
+        int r = 7;
+        double phi = 3.14;
+        double luas = phi * (r * r);
+        System.out.println(luas);
+    }
+}
