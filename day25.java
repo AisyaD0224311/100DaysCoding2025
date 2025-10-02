5c5ccd1d6258bdd797e5c0313780243ecad6456e
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-02 20:00:03 +0800

    Add files via upload

---
+++ day25.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day25 {
+    public static void main(String[] args) {
+        int a = 10;
+        a++; //increment
+        System.out.println(a);
+        
+        a--; //decrement
+        System.out.println(a);
+    }
+}
