91b1e4402abab485be8f5134c5e644457903baad
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-04 15:20:18 +0800

    Add files via upload

---
+++ day27.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day27 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        int p = aa.nextInt();
+        int l = aa.nextInt();
+        int k = p * l / (2 * 2);
+        System.out.println(k);
+    }
+ 
+}
