1444c349925033c2c9b0cc1c5d4f20b674f9895a
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-05 19:49:47 +0800

    Add files via upload

---
+++ day28.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day28 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
