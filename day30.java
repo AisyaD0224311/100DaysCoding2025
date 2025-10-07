8f2cfa59fcaf45878a0bb1416a4413b05d253729
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-07 22:33:13 +0800

    Add files via upload

---
+++ day30.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day30 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        int a = aa.nextInt();
+        int b = aa.nextInt();
+        System.out.println("Apakah " + a + " lebihh besar atau sama dengan " + " ? " + (a >= b));
+        System.out.println("Apakah " + a + " lebihh kecil atau sama dengan " + " ? " + (a <= b));
+    }
+}
