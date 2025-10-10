f7d27c05d056b5a18a958a468203ab3312bbef96
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-10 21:01:31 +0800

    day33.java

---
+++ day33.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day33{
+  public static void main(String []args){
+    boolean a = true;
+    boolean b = false;
+    System.out.println(!a);
+    System.out.println(!b);
+  }
+)
