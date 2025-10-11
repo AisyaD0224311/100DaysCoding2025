44fa59af98870d8e67721a57ee819b365fbb44df
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-11 23:34:06 +0800

    day34.java

--- day34.java
+++ day34.java
@@ -4,8 +4,16 @@ public class day34 {
         int a = 3;
         int b = 5;
         boolean c = true;
+        //gabungan, op logika dan prbandingan
         System.out.println((a > b) && (a < b));
-        System.out.println((a > b) || (a < b));
+        System.out.println((a >= b) || (a <= b));
         System.out.println(!c);
+        //mengubah nilai a dan b
+        a += b;
+        System.out.println(a);
+        b++;
+        System.out.println(b);
+        //perbandingan
+        System.out.println(a != b);
     }
 }
