383a0a11c1e59ef229d2622a30d692ced4f7601d
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-12 19:46:01 +0800

    Add files via upload

---
+++ day35.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day35 {
+    public static void main(String[] args) {
+        int a = 0;
+        if(a == 0){
+            System.out.println("Kamu adalah programmer");
+        }else{
+            System.out.println("Kamu belum jadi programmer, ayo belajar");
+        }
+    }
+}
