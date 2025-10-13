ddc64b253a85e226aadb80c443969167e89c6ba9
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-14 03:43:23 +0800

    Add files via upload

---
+++ day37.java
@@ -0,0 +1,16 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day37 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int a = aa.nextInt();
+        if(a %2 == 0){
+            System.out.println(a + " adalah genap");
+        }else{
+            System.out.println(a + " adalah ganjil");
+        }
+    }
+}
