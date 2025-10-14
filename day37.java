2f34f2bfe3be4b5d5a6588a25b69834da9254fcf
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-14 22:39:20 +0800

    day37.java

--- day37.java
+++ day37.java
@@ -7,10 +7,13 @@ public class day37 {
         Scanner aa = new Scanner(System.in);
         System.out.print("Masukkan angka : ");
         int a = aa.nextInt();
-        if(a %2 == 0){
+        if(a == 0){
+            System.out.println(a + " adalah nol");
+        }else if(a %2 == 0){
             System.out.println(a + " adalah genap");
         }else{
             System.out.println(a + " adalah ganjil");
         }
     }
 }
+
