ece885922be3b81ac32d044c49073e66d9327b29
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-18 13:26:58 +0800

    Implement salary calculation with overtime
    Added main method to calculate total salary including overtime.

--- day41.java
+++ day41.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pkg100dayscoding2025;
-
-/**
- *
- * @author ASUS
- */
 public class day41 {
-    
+    public static void main(String[] args) {
+        Scanner aa =  new Scanner(System.in);
+        int gajiLembur = 55000;
+        System.out.print("Masukkan Gaji Pokok : ");
+        int gaji = aa.nextInt();
+        System.out.print("Masukkan jumlah jam lembur : ");
+        int jam = aa.nextInt();
+        int lembur = gajiLembur * jam;
+        int total = lembur + gaji;
+        System.out.println("Gaji bersih yang diterima : Rp" + total);
+    }
 }
+
