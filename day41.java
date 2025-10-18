44b18ce4a983d78b7ad165588bdb089a41b7bb0d
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-18 21:23:25 +0800

    Update day41.java

--- day41.java
+++ day41.java
@@ -1,20 +1,26 @@
 package pkg100dayscoding2025;
 import java.util.Scanner;
 public class day41 {
-    public static void main(String[] args) {
-        Scanner aa =  new Scanner(System.in);
-        int a = aa.nextInt();
-        if(a %3 == 0 && a %5 == 0){
-            System.out.println("Master class");
-        }else if(a %3 == 0){
-            System.out.println("Pride of 3");
-        }else if(a %5 == 0){
-            System.out.println("Pride of 5");
+   public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        double a = aa.nextInt();
+        double b = aa.nextInt();
+        double c = aa.nextInt();
+        double d = aa.nextInt();
+        double ab = a / b;
+        double cd = c / d;
+        System.out.println(ab);
+        System.out.println(cd);
+        if(ab == cd){
+            System.out.println("Sama");
+        }else if(ab > cd){
+            System.out.println("lebih besar");
         }else{
-            System.out.println("Tidak termasuk kategori");
+            System.out.println("Lebih kecil");
         }
     }
 }
+
 
 
 
