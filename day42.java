8ae65d0365064b1d0f1102b8e7cc3125b2bed3a9
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-19 21:15:25 +0800

    Add files via upload

---
+++ day42.java
@@ -0,0 +1,17 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day42 {
+    public static void main(String[] args) {
+        Scanner aa =  new Scanner(System.in);
+        int gajiLembur = 55000;
+        System.out.print("Masukkan Gaji Pokok : ");
+        int gaji = aa.nextInt();
+        System.out.print("Masukkan jumlah jam lembur : ");
+        int jam = aa.nextInt();
+        int lembur = gajiLembur * jam;
+        int total = lembur + gaji;
+        System.out.println("Gaji bersih yang diterima : Rp" + total);
+    }
+}
