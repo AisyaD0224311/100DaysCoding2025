bc73ebe24fc1d1a396b0e3ecb3d76f732bbf74da
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-27 21:16:02 +0800

    Add day50.java for even or odd number check

---
+++ day50.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day49 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = aa.nextInt();
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Angka " + angka + " adalah " + hasil);
+    }
+}
