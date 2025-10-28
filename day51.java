a0f5f545a32145864e96541fc2d288a911186d4d
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-28 05:28:48 -0700

    Add files via upload

---
+++ day51.java
@@ -0,0 +1,8 @@
+package pkg100dayscoding2025;
+public class day51 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Jw");
+        }
+    }
+}
