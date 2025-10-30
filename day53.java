c2cad788ed7a00b91e335089794bea8ec6d1bc06
AisyaD0224311 <wibusoft59@gmail.com>
2025-10-30 21:16:40 +0800

    Create day53.java

---
+++ day53.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day53 {
+    public static void main(String[] args) {
+         for (int i = 0; i < 10; i++) {
+               if (i == 5) {
+                   break; 
+               }
+               System.out.println(i);
+          }
+    }
+}
