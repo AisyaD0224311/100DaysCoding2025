6a2fbfa0b1244bfcdf191ec25f12a96dade1ef9e
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-08 06:50:15 -0800

    Add files via upload

---
+++ day62.java
@@ -0,0 +1,20 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day62 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan nilai N : ");
+        int N = aa.nextInt();
+        int t = 0;
+        for (int i = 1; i <= N; i++) {
+            t += i;
+            System.out.print(i);
+            if(i < N){
+                System.out.print(" + ");
+            }
+        }
+        System.out.println(" = " + t);
+    }
+}
