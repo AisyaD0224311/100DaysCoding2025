2e8f7c3b8d9c3ae22d7560fcb99fe95abbba05be
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-13 22:11:54 +0800

    Create day67.java

---
+++ day67.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding2025;
+
+import java.util.Scanner;
+
+public class day67 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int a = aa.nextInt();
+        for (int i = 0; i < a; i++) {
+            System.out.println("|");
+        }
+    }
+}
