b3215c49bba3989f250ba448dc5c2dc92129014c
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-15 12:41:41 +0800

    Update day69.java

--- day69.java
+++ day69.java
@@ -1,6 +1,17 @@
 package pkg100dayscoding2025;
+import java.util.Scanner;
 public class day69 {
     public static void main(String[] args) {
-        
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan batas huruf : ");
+        char b = aa.next().charAt(0);
+        System.out.print("Masukkan angka yang dihilangkan : ");
+        char h = aa.next().charAt(0);
+        for (char i = 'A'; i <= b; i++) {
+            if(i != h){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
+
