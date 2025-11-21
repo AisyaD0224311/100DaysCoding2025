30ce423f23fd472e1344dbddf3e5aae8ad151ce2
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-21 22:40:00 +0800

    Add day75.java to demonstrate string methods

---
+++ day75.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding2025;
+public class day75 {
+    public static void main(String[] args) {
+        String teks = "Halo JW";
+        int length = teks.length();
+        String upper = teks.toUpperCase();
+        String lower = teks.toLowerCase();
+        System.out.println("Teks asli      : " + teks);
+        System.out.println("Panjang teks   : " + length);
+        System.out.println("Huruf kapital  : " + upper);
+        System.out.println("Huruf kecil    : " + lower);
+    }
+}
