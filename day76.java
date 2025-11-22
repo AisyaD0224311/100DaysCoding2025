3e8388a5ea60c057e8b83f63589114f2bbad8067
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-22 22:41:54 +0800

    Add day76.java with string comparison methods

---
+++ day76.java
@@ -0,0 +1,13 @@
+package pkg100dayscoding2025;
+public class day75 {
+    public static void main(String[] args) {
+        String a = "Halo JW";
+        String b = "halo jw";
+
+        System.out.println("Apakah " + a + " dan " + b + " sama persis ? : " + a.equals(b));
+        System.out.println("Apakah " + a + " dan " + b + " sama persis tanpa melihat huruf kapital nya ? : " + a.equalsIgnoreCase(b));
+        System.out.println("Apakah " + a + " mengandung kata JW ? : " + a.contains("JW"));
+        System.out.println("Apakah a kosong ? : " + a.isEmpty());
+        System.out.println("Apakah b kosong ? : " + b.isEmpty());
+    }
+}
