275b29e1594574b2432f538ecfc786182b1926c7
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-26 07:15:51 -0800

    Add files via upload

---
+++ day80.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day80 {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        for(int b : a){
+            System.out.println(b);
+        }
+    }
+}
