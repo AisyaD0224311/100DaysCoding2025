80ef84a2ecfe8a8b3e11b045f90ab743009e0007
AisyaD0224311 <wibusoft59@gmail.com>
2025-11-27 21:53:29 +0800

    Create day81.java

---
+++ day81.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding2025;
+public class day81 {
+    public static void main(String[] args) {
+        int[] nilai = {70, 80, 90, 85, 75, 65, 50, 15, 20};
+
+        System.out.println("Nilai indeks 2: " + nilai[2]);
+        System.out.println("Nilai indeks 5: " + nilai[5]);
+        System.out.println("Nilai indeks 7: " + nilai[7]);
+    }
+}
