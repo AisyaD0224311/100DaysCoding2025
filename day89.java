79a5ad4d56d793b13219eefa2e9e5e497e8bd923
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-05 06:51:31 -0800

    Add files via upload

---
+++ day89.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day89 {
+    void tambah(){
+        int hasil = 1 + 2;
+        System.out.println(hasil);
+    }
+    public static void main(String[] args) {
+        day89 tes = new day89();
+        tes.tambah();
+    }
+}
