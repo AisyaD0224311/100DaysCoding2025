799a178c1590730a6c6fb7b2b452a157351d50b3
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-06 22:44:01 +0800

    Add day90 class with addition method

---
+++ day90.java
@@ -0,0 +1,15 @@
+package pkg100dayscoding2025;
+
+public class day90 {
+
+    int tambah() {
+        int hasil = 1 + 2;
+        return hasil;
+    }
+
+    public static void main(String[] args) {
+        day89 tes = new day89();
+        int output = tes.tambah();
+        System.out.println(output);
+    }
+}
