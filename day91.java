e47f70bf9f913798793c34bda5f6eee8ca91bf0e
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-07 04:45:15 -0800

    Add files via upload

---
+++ day91.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding2025;
+public class day91 {
+    void biodata(String nama, int umur, double tinggi){
+        System.out.println("Nama saya " + nama);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi saya " + tinggi);
+    }
+    public static void main(String[] args) {
+        day91 tes = new day91();
+        tes.biodata("Aisya", 18, 150.1);
+    }
+}
