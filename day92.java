ca0a163194907d8bbb70cf96e6112d576e9a9a3b
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-08 06:52:31 -0800

    Add files via upload

---
+++ day92.java
@@ -0,0 +1,14 @@
+package pkg100dayscoding2025;
+public class day92 {
+    int persegi(int sisi){
+        int hasil = sisi * sisi;
+        System.out.println(hasil);
+        return hasil;
+    }
+    
+    public static void main(String[] args) {
+        day92 tes = new day92();
+        tes.persegi(5);
+        
+    }
+}
