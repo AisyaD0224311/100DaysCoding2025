a255a70899da4818a87810309299e43356b023e0
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-09 07:23:06 -0800

    Add files via upload

---
+++ day93.java
@@ -0,0 +1,9 @@
+package pkg100dayscoding2025;
+public class day93 {
+    public static void main(String[] args) {
+        double nilai = 1.1;
+        System.out.println("Ceil : " + Math.ceil(nilai));
+        System.out.println("Floor : " + Math.floor(nilai));
+        System.out.println("Round : " + Math.round(nilai));
+    }
+}
