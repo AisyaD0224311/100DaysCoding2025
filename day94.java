c5ca5571da5d286bea3e51728958071d6122ba3a
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-10 23:31:36 +0800

    Add class to calculate area of a square

---
+++ day94.java
@@ -0,0 +1,12 @@
+package pkg100dayscoding2025;
+public class day94 {
+    void hitungLuas(int sisi) {
+        int luas = sisi * sisi;
+        System.out.println("Luas Persegi adalah: " + luas);
+    }
+
+    public static void main(String[] args) {
+        day94 tes = new day94();
+        tes.hitungLuas(5);
+    }
+}
