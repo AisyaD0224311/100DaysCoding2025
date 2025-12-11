44a4d98011359ae121262f02e1aa5ff9e1fa0ece
AisyaD0224311 <wibusoft59@gmail.com>
2025-12-11 23:27:17 +0800

    Add bio method to display name and age

---
+++ day95.java
@@ -0,0 +1,11 @@
+package pkg100dayscoding2025;
+public class day95 {
+    void bio(String nama, int umur) {
+        System.out.println("Nama saya adalah: " + nama + " dengan umur " + umur);
+    }
+
+    public static void main(String[] args) {
+        day95 tes = new day95();
+        tes.bio("Aisya", "18");
+    }
+}
